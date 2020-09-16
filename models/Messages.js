773c7a55966066c1b05d5bbf7a21ff3c8ca6eaf5
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-16 23:13:26 +0200

    edit messages schema

--- models/Messages.js
+++ models/Messages.js
@@ -6,7 +6,11 @@ const UserSchema = new Schema({
   parent_id: { type: String, required: true },
   Post_date: { type: date, default: Date.now() },
   list_receivers: { type: Array, required: true, minlength: 1 },
-  Auther: [String],
+  Auther: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+    required: 15,
+  },
   title: { type: String, required: true },
   body: { type: String, required: true },
 });
