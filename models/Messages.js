656056f778b4c38db5ad83e59b3d323d7ee8d147
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-16 23:08:35 +0200

    adding Messages schema model

---
+++ models/Messages.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  parent_id: { type: String, required: true },
+  Post_date: { type: date, default: Date.now() },
+  list_receivers: { type: Array, required: true, minlength: 1 },
+  Auther: [String],
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
