7d69f1eb2b7f9bde2acd2c2d87a136ecd31db8ea
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-17 07:09:12 +0200

    adding user route  to app routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const user = require('./routes/users');
 const app = express();
 
 app.use(express.json());
@@ -11,10 +11,13 @@ mongoose
   .connect(db, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true,
   })
   .then(() => console.log('database connected...'))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/users', user);
+
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log('server is running'));
