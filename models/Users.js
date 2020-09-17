9755bdab2fff8e452499b9bc74d3a94583d9e50c
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-17 07:11:33 +0200

    using bcrypt to hash password in users schema

--- models/Users.js
+++ models/Users.js
@@ -3,10 +3,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    set: (n) => {
+      var splitStr = n.split(' ');
+      for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] =
+          splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+      }
+      return splitStr.join(' ');
+    },
+  },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  mobile: { type: String, minlength: 10, maxlength: 15, unique: true },
+  mobile: { type: String },
   dob: { type: Date },
   register_date: { type: Date, default: Date.now() },
 });
