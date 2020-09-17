a5f423f33c9485cb83f40042db32956b34dbd172
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-17 10:12:46 +0200

    adding jwt to login process

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -3,19 +3,29 @@ const User = require('../../models/Users');
 
 module.exports = (req, res) => {
   const { email, password } = req.body;
-  User.findOne({ email }).then((user) => {
-    if (!user)
-      return res.status(400).json({
-        msg: `Invalid email or password`,
-      });
-
-    bcrypt.compare(password, user.password).then((validPass) => {
-      if (!validPass)
+  User.findOne({ email })
+    // .select('-password')
+    .then((user) => {
+      if (!user)
         return res.status(400).json({
           msg: `Invalid email or password`,
         });
 
-      return res.json({ msg: 'ok' });
+      bcrypt.compare(password, user.password).then((validPass) => {
+        if (!validPass)
+          return res
+            .status(400)
+            .json({
+              msg: `Invalid email or password`,
+            })
+            .catch((err) => console.log(err));
+
+        let outUser = user.toObject();
+        delete outUser['password'];
+
+        const authToken = user.generateAuthTocken();
+
+        return res.json({ user: outUser, authToken });
+      });
     });
-  });
 };
