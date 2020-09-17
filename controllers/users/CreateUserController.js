86754cc9b1e93317df2c08a3afa4ebb3c79e19b6
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-17 07:09:59 +0200

    add controller for creating new user

---
+++ controllers/users/CreateUserController.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+const User = require('../../models/Users');
+
+module.exports = (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (user)
+      return res.status(400).json({
+        msg: `this email ${email} is already exist \n try to sign in instead`,
+      });
+  });
+
+  const newUser = new User({ name, email, password });
+
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(newUser.password, salt))
+    .then((hash) => {
+      newUser.password = hash;
+      newUser
+        .save()
+        .then((user) =>
+          res.json({ id: user.id, name: user.name, email: user.email })
+        );
+    })
+    .catch((err) => console.log(err));
+};
