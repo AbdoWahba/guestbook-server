1551c90452e7b0543f7fddbabee839edadbfef96
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-18 00:00:49 +0200

    fixing some bugs in model

--- models/Messages.js
+++ models/Messages.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
-
+const Users = require('./Users');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
   parent_id: { type: String, required: true },
-  Post_date: { type: date, default: Date.now() },
+  post_date: { type: Date, default: Date.now() },
   list_receivers: { type: Array, required: true, minlength: 1 },
-  Auther: {
+  auther: {
     type: Schema.Types.ObjectId,
-    ref: 'Users',
+    ref: Users,
     required: true,
   },
   title: { type: String, required: true },
   body: { type: String, required: true },
 });
 
-module.exports = Message = mongoose.model('user', MessageSchema);
+module.exports = Message = mongoose.model('message', MessageSchema);
