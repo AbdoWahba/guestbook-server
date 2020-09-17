9cdf28338a67c3545baac596e9d7f9dddff040c3
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-18 00:05:49 +0200

    adding middleware for cros

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./routes/users');
 const auth = require('./routes/auth');
+const messages = require('./routes/messages');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const db = require('./config/keys').mongoURI;
 
@@ -19,7 +33,8 @@ mongoose
 
 app.use('/api/users', user);
 app.use('/api/auth', auth);
+app.use('/api/messages', messages);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('server is running'));
