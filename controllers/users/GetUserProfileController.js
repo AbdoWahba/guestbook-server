1d7f5656560f4959c5297475d30b1d36e7722dc2
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-18 00:05:27 +0200

    add more validation to get user

--- controllers/users/GetUserProfileController.js
+++ controllers/users/GetUserProfileController.js
@@ -1,7 +1,10 @@
 const User = require('../../models/Users');
 
 module.exports = (req, res) => {
-  User.findById(req.user.id)
+  User.findById(req.user._id)
     .select('-password')
-    .then((user) => res.json(user));
+    .then((user) => {
+      if (!user) return res.status(404).json({ msg: 'user not found' });
+      return res.json({ user });
+    });
 };
